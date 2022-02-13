1965da9a0eb6b307c73838abd442f9421a953f89
Armand Kulsh <zenars85@mail.ru>
2022-02-13 20:03:41 +0300

    Fix workflow that runs crawler tests

--- .github/ui_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip3 install -r requirements.txt -r requirements-test.txt
-python -m unittest discover -s tests/
-coverage run -m unittest discover -s tests/
-coverage report --include ui/ui.py > output/test/ui_report.txt
